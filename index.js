d7bfaaefbbbe085fdc720527024f260564a18d12
Genevieve <genschultz@gmail.com>
2019-01-15 22:53:05 -0800

    removeDuplicateWords and removeSmallest solutions

--- index.js
+++ index.js
@@ -38,3 +38,102 @@ Test.describe("Basic tests", function () {
     Test.assertSimilar(tribonacci([300, 200, 100], 0), [])
     Test.assertSimilar(tribonacci([0.5, 0.5, 0.5], 30), [0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5])
 })
+
+
+// Remove duplicate words
+
+// Your task is to remove all duplicate words from string, leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+
+function removeDuplicateWords(s) {
+
+    var arr = s.split(" ");
+    var words = [];
+    for (i = 0; i < arr.length; i++) {
+        //     console.log(words);
+        var current = arr[i].replace(/[,]/g, "");
+        if (!words.includes(current)) {
+            words.push(current);
+        }
+    }
+    var result = words.join(' ')
+    return result;
+}
+
+// Tests
+Test.describe('Example tests', _ => {
+    Test.it('should handle example case', _ => {
+        Test.assertEquals(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'), 'alpha beta gamma delta');
+    });
+});
+
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// removeSmallest([1,2,3,4,5]) = [2,3,4,5]
+// removeSmallest([5,3,2,1,4]) = [5,3,2,4]
+// removeSmallest([2,2,1,2,1]) = [2,2,2,1]
+
+function removeSmallest(numbers) {
+
+    var result = numbers.slice(0);
+    var lowestNumber = result[0];
+    var lowestLoc = 0;
+    result.forEach(function (item, i) {
+        console.log(`the smallest number is ${lowestNumber} at index ${lowestLoc}`)
+        if (item < lowestNumber) {
+            lowestNumber = item;
+            lowestLoc = i;
+        }
+    });
+    result.splice(lowestLoc, 1);
+    return result;
+}
+
+// Tests
+Test.describe("removeSmallest", function () {
+    Test.it("works for the examples", function () {
+        Test.assertSimilar(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
+        Test.assertSimilar(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]");
+        Test.assertSimilar(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]");
+        Test.assertSimilar(removeSmallest([]), [], "Wrong result for []");
+    });
+
+    Test.it("returns [] if the list has only one element", function () {
+        for (let i = 0; i < 10; ++i) {
+            let x = ~~(Math.random() * 400);
+            Test.assertSimilar(removeSmallest([x]), [], `Wrong result for ${[x]}`);
+        }
+    });
+
+    function randomArray(length) {
+        return Array.from({
+            length: length
+        }, () => ~~(Math.random() * 400));
+    }
+
+    Test.it("returns a list that misses only one element", function () {
+        for (let i = 0; i < 10; ++i) {
+            let arr = randomArray(~~(Math.random() * 10) + 1);
+            let l = arr.length;
+            Test.assertSimilar(removeSmallest(arr).length, l - 1, `Wrong result for ${arr}`);
+        }
+    });
+});
