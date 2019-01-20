1c8302618ff97a4f76908ab6255ca71e04d08e1f
Genevieve <genschultz@gmail.com>
2019-01-20 14:50:39 -0800

    duplicateCount problem solved

--- index.js
+++ index.js
@@ -495,3 +495,54 @@ Test.describe("FindEvenIndex", function() {
     );
   });
 });
+
+/* Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice */
+
+function duplicateCount(text) {
+  // convert all characters to lowercase and copy string to an array, separated by character
+  const char = text.toLowerCase().split("");
+  // empty object to store counts of each unique character
+  var charCount = {};
+  // count of duplicates
+  var dupCount = 0;
+
+  // add characters key value pairs to object
+  char.forEach((item, i) => {
+    if (item in charCount) {
+      charCount[item] = charCount[item] + 1;
+    } else {
+      charCount[item] = 1;
+    }
+  });
+
+  // if counts of characters are more than 1, increase duplicates count
+  for (var key in charCount) {
+    let value = charCount[key];
+    if (value > 1) {
+      dupCount++;
+    }
+  }
+  return dupCount;
+}
+
+// Tests
+Test.assertEquals(duplicateCount(""), 0);
+Test.assertEquals(duplicateCount("abcde"), 0);
+Test.assertEquals(duplicateCount("aabbcde"), 2);
+Test.assertEquals(duplicateCount("aabBcde"), 2, "should ignore case");
+Test.assertEquals(duplicateCount("Indivisibility"), 1);
+Test.assertEquals(
+  duplicateCount("Indivisibilities"),
+  2,
+  "characters may not be adjacent"
+);
